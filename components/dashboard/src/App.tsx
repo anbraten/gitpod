b557ddc1674ad3570b623832075dd153ba6cc3f4
Jan Keromnes <janx@linux.com>
2022-02-04 15:52:20 +0000

    [dashboard] Implement a PaymentContext and use it to hide payment features when payment is disabled

--- components/dashboard/src/App.tsx
+++ components/dashboard/src/App.tsx
@@ -298,8 +298,8 @@ function App() {
 
                 <Route path="/admin/users" component={UserSearch} />
                 <Route path="/admin/workspaces" component={WorkspacesSearch} />
-                <Route path="/admin/settings" component={AdminSettings} />
                 <Route path="/admin/projects" component={ProjectsSearch} />
+                <Route path="/admin/settings" component={AdminSettings} />
 
                 <Route path={["/", "/login"]} exact>
                     <Redirect to={workspacesPathMain} />
@@ -326,6 +326,9 @@ function App() {
                     <Route exact path={projectsPathMain} component={Projects} />
                     <Route exact path={projectsPathMainWithParams} render={(props) => {
                         const { resourceOrPrebuild } = props.match.params;
+                        if (resourceOrPrebuild === "prebuilds") {
+                            return <Prebuilds />;
+                        }
                         if (resourceOrPrebuild === "settings") {
                             return <ProjectSettings />;
                         }
@@ -334,9 +337,6 @@ function App() {
                         }
                         if (resourceOrPrebuild === "variables") {
                             return <ProjectVariables />;
-                        }
-                        if (resourceOrPrebuild === "prebuilds") {
-                            return <Prebuilds />;
                         }
                         return resourceOrPrebuild ? <Prebuild /> : <Project />;
                     }} />
@@ -365,6 +365,9 @@ function App() {
                             if (maybeProject === "settings") {
                                 return <TeamSettings />;
                             }
+                            if (resourceOrPrebuild === "prebuilds") {
+                                return <Prebuilds />;
+                            }
                             if (resourceOrPrebuild === "settings") {
                                 return <ProjectSettings />;
                             }
@@ -373,9 +376,6 @@ function App() {
                             }
                             if (resourceOrPrebuild === "variables") {
                                 return <ProjectVariables />;
-                            }
-                            if (resourceOrPrebuild === "prebuilds") {
-                                return <Prebuilds />;
                             }
                             return resourceOrPrebuild ? <Prebuild /> : <Project />;
                         }} />
